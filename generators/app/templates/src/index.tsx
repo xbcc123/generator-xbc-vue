3447db3ba8dd477bb027f1bce6406470f47b60c6
鄢康杰 <287207951@qq.com>
2020-10-07 16:43:12 +0800

    chore: cdn改为本地静态资源

--- generators/app/templates/src/index.tsx
+++ generators/app/templates/src/index.tsx
@@ -5,6 +5,7 @@ import router from "./router"
 import store from "./store"
 import "./services"
 
+
 const isDebug_mode = process.env.NODE_ENV !== "production"
 Vue.config.debug = isDebug_mode
 Vue.config.devtools = isDebug_mode
